282f45596a8a8aa6fc146063578d2f23897849b3
NabilRaiyan <rraiyan77@gmail.com>
2024-10-22 20:32:36 +0600

    creating sing in option

--- src/auth/dto/signinDto.ts
+++ src/auth/dto/signinDto.ts
@@ -1,6 +1,11 @@
-import { IsEmail, Matches, IsNotEmpty } from "class-validator"
+import { IsEmail, Matches, IsNotEmpty } from 'class-validator';
 
-export class SignInDto() {
-
-
+export class SignInDto {
+  @IsNotEmpty({ message: 'Please Enter Email' })
+  @IsEmail({}, { message: 'Email must be a valid email address' })
+  @Matches(/^[a-zA-Z0-9._%+-]+@gmail\.com$/, {
+    message: 'Email must be a valid Gmail address',
+  })
+  email: string;
+  
 }
