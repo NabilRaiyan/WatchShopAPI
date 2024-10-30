bdfbd99616db8a99a472fe2d13584231b0d6eeac
NabilRaiyan <rraiyan77@gmail.com>
2024-10-30 18:51:56 +0600

    creating accessories and gift box dto

--- src/product/dto/accessory.dto.ts
+++ src/product/dto/accessory.dto.ts
@@ -96,3 +96,71 @@ export class UpdateAccessoryDto {
   @IsOptional()
   categoryId?: number;
 }
+
+// Gift box dto
+export class CreateGiftBoxDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsDecimal()
+  price: number;
+
+  @IsString()
+  @IsNotEmpty()
+  currency: string;
+
+  @IsString()
+  @IsNotEmpty()
+  stock_status: string;
+
+  @IsString()
+  @IsNotEmpty()
+  contents: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  categoryId: number;
+
+  @IsArray()
+  @IsOptional()
+  imageIds: number[];
+}
+
+export class UpdateGiftBoxDto {
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsDecimal()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  currency?: string;
+
+  @IsString()
+  @IsOptional()
+  stock_status?: string;
+
+  @IsString()
+  @IsOptional()
+  contents?: string;
+
+  @IsNumber()
+  @IsOptional()
+  categoryId?: number;
+
+  @IsArray()
+  @IsOptional()
+  imageIds?: number[];
+}
