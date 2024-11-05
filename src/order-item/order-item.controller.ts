b6ac0397b29c388bd99cb91bee15abccfa762760
NabilRaiyan <rraiyan77@gmail.com>
2024-11-05 21:18:19 +0600

    creating accessories insert method

--- src/order-item/order-item.controller.ts
+++ src/order-item/order-item.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { OrderItemService } from './order-item.service';
 
-// 
+// Order item controller
 @Controller('order-item')
 export class OrderItemController {
   constructor(private readonly orderItemService: OrderItemService) {}
