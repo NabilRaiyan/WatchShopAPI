809f98f37936d0caa3b146b0d74b745c3f9174a4
NabilRaiyan <rraiyan77@gmail.com>
2024-11-14 22:17:58 +0600

    creating image url for accesories

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -24,6 +24,7 @@ export interface ProductResponse {
   saveImage: ProductImageEntity;
 }
 
+// accessories response
 export interface AccessoryResponse {
   accessories: AccessoryEntity;
   saveImage: ProductImageEntity;
