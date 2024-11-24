cbbedbf6dde97eb99a56af5256a8b70d2696ff5d
NabilRaiyan <rraiyan77@gmail.com>
2024-11-24 21:44:32 +0600

    adding relationship to the cartitem entity

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -4,7 +4,7 @@ import { CategoryEntity } from './category.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-// category service
+// category 
 @Injectable()
 export class CategoryService {
   constructor(
