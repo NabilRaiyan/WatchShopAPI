bd21d534ffa2f71f2fa36fae7828d85e84506e98
NabilRaiyan <rraiyan77@gmail.com>
2024-11-27 21:00:56 +0600

    adding likes into like servise

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductEntity } from 'src/product/entity';
+import { UserEntity } from 'src/user/entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class LikeService {}
+export class LikeService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(ProductEntity)
+    private readonly productRepository: Repository<ProductEntity>,
+  ) {}
+}
