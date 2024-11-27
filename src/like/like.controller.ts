2684ce419e601086edf3599493eb4ce50c0ca4c6
NabilRaiyan <rraiyan77@gmail.com>
2024-11-27 20:57:49 +0600

    adding relationship to the like entity

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { LikeService } from './like.service';
 
+// like controller
 @Controller('like')
 export class LikeController {
   constructor(private readonly likeService: LikeService) {}
