4c1e63c7c72508763ae9262d73bc4fa5a91bc9dc
NabilRaiyan <rraiyan77@gmail.com>
2024-12-02 22:25:47 +0600

    creating checkout method

--- src/cart-item/cart.dto.ts
+++ src/cart-item/cart.dto.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty, IsInt, Min } from 'class-validator';
 
+// cart dto class
 export class CartItemDto {
   @IsNotEmpty({ message: 'Please provide the quantity of the item.' })
   @IsInt({ message: 'Quantity must be an integer.' })
